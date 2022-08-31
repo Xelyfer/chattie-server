3f03635a7c9fe4697b083307f4997d5a198a48d4
Xelyfer <Xelyfer@gmail.com>
2022-08-31 20:44:24 +1200

    Added proper room function and cross-origin

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const origin = "https://chattie-xelyfer.herokuapp.com/";
 const io = require("socket.io")(http, {
   cors: {
-    origin: [origin],
+    origin: "https://chattie-xelyfer.herokuapp.com",
+    // origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`${socket.id} has connected`);
-  socket.on("message", ({ name, message }) => {
+  socket.on("join", ({ qRoom }) => {
+    socket.join(qRoom);
+    console.log(`Joined room: ${qRoom}`);
+  });
+  socket.on("message", ({ name, message, room }) => {
     console.log(`Sending message: Name: ${name}, Message: ${message}`);
-    io.emit("message", { name, message });
+    io.to(room).emit("message", { name, message });
   });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 http.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
